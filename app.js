30a3e048e74acc50c3cf4c112468ebc5e6db7eee
sheldon jones <sheldon.jones@mymona.uwi.edu>
2025-11-20 21:43:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const searchBtn = document.getElementById('searchBtn');
 
-    // Q3: Added search input and result div variables
+    
     const searchInput = document.getElementById('searchInput');
     const resultDiv = document.getElementById('result');
 
